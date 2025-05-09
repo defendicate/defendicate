8bb17db9f33907460ed6598921b2c74d99933e68
Defendicate <5defendicate@gmail.com>
2025-05-09 22:54:31 +0100

    update

--- firebaseauth.js
+++ firebaseauth.js
@@ -81,7 +81,7 @@
         showMessage('login is successful', 'signInMessage');
         const user=userCredential.user;
         localStorage.setItem('loggedInUserId', user.uid);
-        window.location.href='homepage.html';
+        window.location.href='home.html';
     })
     .catch((error)=>{
         const errorCode=error.code;
